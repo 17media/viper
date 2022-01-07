4a02764884e4e843f5172770842385d2b37b46f2
Yap <cheekian.yap@aiesec.net>
2022-01-07 13:34:38 +0800

    fix imports to 17media (#2)

--- viper.go
+++ viper.go
@@ -40,14 +40,14 @@ import (
 	"github.com/spf13/cast"
 	"github.com/spf13/pflag"
 
-	"github.com/spf13/viper/internal/encoding"
-	"github.com/spf13/viper/internal/encoding/dotenv"
-	"github.com/spf13/viper/internal/encoding/hcl"
-	"github.com/spf13/viper/internal/encoding/ini"
-	"github.com/spf13/viper/internal/encoding/javaproperties"
-	"github.com/spf13/viper/internal/encoding/json"
-	"github.com/spf13/viper/internal/encoding/toml"
-	"github.com/spf13/viper/internal/encoding/yaml"
+	"github.com/17media/viper/internal/encoding"
+	"github.com/17media/viper/internal/encoding/dotenv"
+	"github.com/17media/viper/internal/encoding/hcl"
+	"github.com/17media/viper/internal/encoding/ini"
+	"github.com/17media/viper/internal/encoding/javaproperties"
+	"github.com/17media/viper/internal/encoding/json"
+	"github.com/17media/viper/internal/encoding/toml"
+	"github.com/17media/viper/internal/encoding/yaml"
 )
 
 // ConfigMarshalError happens when failing to marshal the configuration.
@@ -1841,7 +1841,7 @@ func (v *Viper) WatchRemoteConfigOnChannel() error {
 // Retrieve the first found remote configuration.
 func (v *Viper) getKeyValueConfig() error {
 	if RemoteConfig == nil {
-		return RemoteConfigError("Enable the remote features by doing a blank import of the viper/remote package: '_ github.com/spf13/viper/remote'")
+		return RemoteConfigError("Enable the remote features by doing a blank import of the viper/remote package: '_ github.com/17media/viper/remote'")
 	}
 
 	for _, rp := range v.remoteProviders {
